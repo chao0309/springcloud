b2aaa90099b3980bf0a295c0b1dee68ad18229e5
wang chao <2427671947@qq.com>
2020-04-15 22:07:26 +0800

    修改

--- ShejiMoShi-learn/src/main/java/cn/how2j/springcloud/test/Test.java
+++ ShejiMoShi-learn/src/main/java/cn/how2j/springcloud/test/Test.java
@@ -1,7 +1,22 @@
 package cn.how2j.springcloud.test;
 
-public class Test {
-    public static void main(String[] args) {
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Vector;
 
+public class Test {
+    public static void main(String[] args) throws  Exception{
+        Uesr uesr = new Uesr("wangchao",18);
+        Class cl = uesr.getClass();
+        Method[] methods = cl.getMethods();
+        for(Method m : methods){
+            System.out.println(m.toString());
+        }
+        Field[] fields = cl.getDeclaredFields();
+        Field field = cl.getDeclaredField("name");
+        field.setAccessible(true);
+        field.set(uesr,"waa");
+        System.out.println(uesr.getName());
     }
 }
